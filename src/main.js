ebc64603c729e78320b9782ac9ac1e713d13911c
qiushiming <qiushiming1997@gmail.com>
2020-12-27 21:25:28 +0800

    init

--- src/main.js
+++ src/main.js
@@ -3,4 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import "@/element-plus/lib/theme-chalk/src/index.scss";
+
 createApp(App).use(store).use(router).mount('#app')
